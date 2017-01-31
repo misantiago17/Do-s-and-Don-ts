9844f20d65fccfe009768d30bde8ac790cba867a
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2017-01-31 11:16:06 -0200

    Initial Commit

---
+++ Do's and Don'ts/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Do's and Don'ts
+//
+//  Created by Michelle Beadle on 25/01/17.
+//  Copyright © 2017 Michelle Beadle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewTodo: UIView!
+    @IBOutlet weak var viewTantoEmTanto: UIView!
+    @IBOutlet weak var viewEspecifico: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func mudaRotina(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.viewTodo.alpha = 1
+                self.viewTantoEmTanto.alpha = 0
+                self.viewEspecifico.alpha = 0
+            })
+        } else if sender.selectedSegmentIndex == 1 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.viewTodo.alpha = 0
+                self.viewTantoEmTanto.alpha = 1
+                self.viewEspecifico.alpha = 0
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.viewTodo.alpha = 0
+                self.viewTantoEmTanto.alpha = 0
+                self.viewEspecifico.alpha = 1
+            })
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
